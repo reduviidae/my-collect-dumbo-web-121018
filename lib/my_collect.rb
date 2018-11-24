889d99d285425b41a63fcf6d944d0c7d19592b2f
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 15:11:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
-require 'pry'
+# require 'pry'
 
 def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
+  # binding.pry
     yield(array[i])
     new_array << yield(array[i])
     i += 1
