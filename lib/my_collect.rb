507e68b093ff2aac4f1fa74f46d62537f0e04f21
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 15:11:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,16 @@
+# require 'pry'
+
 def my_collect(array)
   i = 0
+  new_array = []
   while i < array.length
-    yield array[i]
+  # binding.pry
+    yield(array[i])
+    new_array << yield(array[i])
     i += 1
   end
+  new_array
 end
+
+
 
