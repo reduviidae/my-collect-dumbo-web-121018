7bc8bc6868cea8366174e0478556d87ea1a5acad
Wil Cook <wil.cook.is@gmail.com>
2018-11-24 14:29:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,13 @@
 def my_collect(array)
   i = 0
+  new_array = []
   while i < array.length
-    yield array[i]
+    yield(array[i])
+    new_array << array[i]
     i += 1
   end
+  new_array
 end
+
+
 
